b0ff8b0668ab960ed97ea6fbdcf91c83b7760ddb
NGUYEN-XUAN Phuc <pnguyenxuan@murex.com>
2019-04-17 18:25:36 +0200

    navbar bagel commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/bagel', function(req, res, next) {
+  res.render('bagel', { title: 'Welcome to Bagel' });
+});
+
 module.exports = router;
